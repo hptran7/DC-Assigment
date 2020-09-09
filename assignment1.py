7236fad5a53970285230595cb90971ea6db77667
Tran Hung <tranhung@Trans-MacBook-Air.local>
2020-09-09 12:50:49 -0500

    First commit

--- assignment1.py
+++ assignment1.py
@@ -11,3 +11,24 @@
 # Based on the operand you are going to perform the math operation and print the result on the terminal. 
 
 # - Make sure each math operation is a separate function. 
+
+def add(num1, num2):
+    return num1 +num2
+def sub(num1,num2):
+    return num1- num2
+def mul(num1, num2):
+    return num1 * num2
+def div(num1, num2):
+    return num1/num2
+    
+number1 = float(input("Please enter your first number: "))
+number2 = float(input("Please enter your second number: "))
+operand = input("Please enter you operand: ")
+if operand == "+":
+    print(add(number1,number2))
+elif operand == "-":
+    print(sub(number1,number2))
+elif operand =="*":
+    print(mul(number1,number2))
+elif operand =="/":
+    print(div(number1,number2))
---
+++ assignment3.py
@@ -0,0 +1,12 @@
+# Take input from the user. If the input is divisible by 3 
+# then print "Fizz", if the input it divisible by 5 then print "Buzz". 
+# If the input is divisible by 3 and 5 then print "Fizz Buzz". 
+num = float(input("Please enter your number: "))
+if num%3 == 0 and num%5 != 0:
+    print("Fizz")
+elif num%3 !=0 and num%5 ==0:
+    print("Buzz")
+elif num%3 ==0 and num%5 ==0:
+    print("Fizz Buzz")
+else:
+    print("Your number is not correct") 
