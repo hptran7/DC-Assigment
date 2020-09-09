f7eeda61a411622cae68d404e2d1006d2921eb1e
Tran Hung <tranhung@Trans-MacBook-Air.local>
2020-09-09 15:35:57 -0500

    New Assignment 3

--- assignment3.py
+++ assignment3.py
@@ -2,7 +2,7 @@
 # then print "Fizz", if the input it divisible by 5 then print "Buzz". 
 # If the input is divisible by 3 and 5 then print "Fizz Buzz". 
 num = float(input("Please enter your number: "))
-while num%3 != 0 or num%5 != 0:
+while num%3 != 0 and num%5 != 0:
     num = float(input("Please enter a different number: "))
 if num%3 == 0 and num%5 != 0:
     print("Fizz")
