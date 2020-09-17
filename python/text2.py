85732cfb373ccf405ce7df75a1a462df1a1d3b08
Tran Hung <tranhung@Trans-MacBook-Air.local>
2020-09-17 14:01:09 -0500

    Organize files

---
+++ python/text2.py
@@ -0,0 +1,9 @@
+class chicken:
+    def __init__(self,species,name, eggs =0):
+        self.species = species
+        self.name = name
+        self.eggs = eggs
+
+    def lay_egg(self,number):
+        self.total_eggs += 1
+        return 
